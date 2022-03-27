3cf7c57e13334fcfef0d140b7388793c36772b12
Yonge Bai <yongebai@gmail.com>
2022-03-27 19:25:12 -0400

    only face detection

--- face-detection.py
+++ face-detection.py
@@ -12,9 +12,6 @@ detector = dlib.get_frontal_face_detector()
 
 dlib_facelandmark = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
 
-left_eye = list(range(43, 48))
-right_eye = list(range(37, 43))
-
 while True:
     _, frame = capture.read()
     gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
@@ -24,14 +21,10 @@ while True:
     for face in faces:
         face_landmarks = dlib_facelandmark(gray, face)
 
-        for i,j in zip(left_eye, right_eye):
-            left_x = face_landmarks.part(i).x
-            left_y = face_landmarks.part(i).y
-            cv.circle(frame, (left_x, left_y), 1, (255, 0, 255), 2)
-
-            right_x = face_landmarks.part(j).x
-            right_y = face_landmarks.part(j).y
-            cv.circle(frame, (right_x, right_y), 1, (255, 0, 255), 2)
+        for n in range(0,68):
+            x = face_landmarks.part(n).x
+            y = face_landmarks.part(n).y
+            cv.circle(frame, (x, y), 1, (255, 0, 255), 2)
 
     cv.imshow("Face detection", cv.flip(frame, 1))
     key = cv.waitKey(1)
